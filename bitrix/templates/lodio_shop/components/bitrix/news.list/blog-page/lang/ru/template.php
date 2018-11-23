82ed6e466e104716c09dd0c3d7c8119013942536
Unknown <alexmelkov@mail.ru>
2018-11-23 13:39:59 +0300

    Переименование категорий статей в Блоге

--- bitrix/templates/lodio_shop/components/bitrix/news.list/blog-page/lang/ru/template.php
+++ bitrix/templates/lodio_shop/components/bitrix/news.list/blog-page/lang/ru/template.php
@@ -3,8 +3,8 @@ $MESS ['T_LODIO_OUR'] = "Наш";
 $MESS ['T_LODIO_BLOG'] = "Блог";
 $MESS ['T_LODIO_ALLCATEGORIES'] = "Все";
 $MESS ['T_LODIO_SORT_CAT_1'] = "О разном";
-$MESS ['T_LODIO_SORT_CAT_2'] = "Новая Коллекция";
-$MESS ['T_LODIO_SORT_CAT_3'] = "Новинки Мужской и Женской одежды";
-$MESS ['T_LODIO_SORT_CAT_4'] = "Онлайн Экслюзив";
-$MESS ['T_LODIO_SORT_CAT_5'] = "Распродажа";
+$MESS ['T_LODIO_SORT_CAT_2'] = "Мода";
+$MESS ['T_LODIO_SORT_CAT_3'] = "Мужчинам";
+$MESS ['T_LODIO_SORT_CAT_4'] = "Женщинам";
+$MESS ['T_LODIO_SORT_CAT_5'] = "Аксессуары";
 ?>
