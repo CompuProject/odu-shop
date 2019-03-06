1d60ab5d6cffe7cc3ffd3d86cc06af098aff7b88
Unknown <alexmelkov@mail.ru>
2019-03-06 19:55:18 +0300

    Деактивируем ненужные каталоги

--- cp_lib/EventHandlers/OnBeforeIBlockElementUpdate.php
+++ cp_lib/EventHandlers/OnBeforeIBlockElementUpdate.php
@@ -2,8 +2,9 @@
 include_once $_SERVER["DOCUMENT_ROOT"] . "/cp_lib/Helpers/CP_GetGlobalParam.php";
 include_once $_SERVER["DOCUMENT_ROOT"] . "/cp_lib/Helpers/CP_CatalogHelper.php";
 
-AddEventHandler("iblock", "OnBeforeIBlockElementAdd", ["CP_ActivateUpdater", "init"]);
-AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", ["CP_ActivateUpdater", "init"]);
+AddEventHandler("iblock", "OnBeforeIBlockElementAdd", ["CP_ActivateUpdater", "productUpdate"]);
+AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", ["CP_ActivateUpdater", "productUpdate"]);
+AddEventHandler("iblock", "OnBeforeIBlockSectionUpdate", ["CP_ActivateUpdater", "catalogUpdate"]);
 
 class CP_ActivateUpdater
 {
@@ -12,7 +13,7 @@ class CP_ActivateUpdater
         "Нет" => "N",
     ];
 
-    public static function init(&$arFields)
+    public static function productUpdate(&$arFields)
     {
         if ($arFields["IBLOCK_ID"] != CP_CatalogHelper::getShopIblockId()) {
             return;
@@ -22,5 +23,11 @@ class CP_ActivateUpdater
         $arFields["ACTIVE"] = static::$activnost[$arFields["PROPERTY_VALUES"]["95"][$activity_key]["VALUE"]];
         $section_key = key($arFields["IBLOCK_SECTION"]);
         $arFields["IBLOCK_SECTION"][$section_key] = CP_CatalogHelper::convertCatalogXMLIdtoID(CP_CatalogHelper::getEnumPropertyValueFromId($arFields["PROPERTY_VALUES"]["96"][$parent_catalog_key]["VALUE"]));
+    }
+
+    public static function catalogUpdate (&$arFields) {
+        if ($arFields["XML_ID"] == "08aed71b-8540-11e8-a478-f81654daadda" || $arFields["XML_ID"] == "08aed71d-8540-11e8-a478-f81654daadda") {
+            $arFields["ACTIVE"] = "N";
+        }
     }
 }
