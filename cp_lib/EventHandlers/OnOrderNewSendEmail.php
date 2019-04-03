c4c301de653cb913b89cca706a155f77f4745562
Unknown <alexmelkov@mail.ru>
2019-04-03 22:17:30 +0300

    Добавление бренда в письмо клиенту

--- cp_lib/EventHandlers/OnOrderNewSendEmail.php
+++ cp_lib/EventHandlers/OnOrderNewSendEmail.php
@@ -23,6 +23,7 @@ class CP_OnOrderNewSendEmail
                 $arrayRes[$counter]["PRODUCT_CODE"] = CP_CatalogHelper::getPropertyValue($arProps["PRODUCT_ID"],"CML2_ARTICLE");
                 $arrayRes[$counter]["PRODUCT_ID"] = $arProps["PRODUCT_ID"];
                 $arrayRes[$counter]["NAME"] = $arProps["NAME"];
+                $arrayRes[$counter]["BRAND"] = CP_CatalogHelper::getEnumPropertyCodeFromId(CP_CatalogHelper::getPropertyValue($arProps["PRODUCT_ID"],"BRAND"));
                 $arrayRes[$counter]["QUANTITY"] = $arProps["QUANTITY"];
                 $arrayRes[$counter]["PRICE"] = $arProps["PRICE"];
                 $arrayRes[$counter]["SUMM"] = $arProps["QUANTITY"] * $arProps["PRICE"];
@@ -31,13 +32,13 @@ class CP_OnOrderNewSendEmail
             $strOrderList .= '<table class="orderTable">';
             $strOrderList .= '<tr>';
             $strOrderList .= '<th>Код товара</th>';
-            $strOrderList .= '<th>Наименование</th>';
+            $strOrderList .= '<th>Бренд/Наименование</th>';
             $strOrderList .= '<th>Количество</th>';
             $strOrderList .= '<th>Цена</th>';
             $strOrderList .= '<th>Сумма</th>';
             $strOrderList .= '</tr>';
             foreach ($arrayRes as $item) {
-                $strOrderList .= '<tr><td>'.$item["PRODUCT_CODE"].'</td><td>'.$item["NAME"].'</td><td>'.$item["QUANTITY"].'</td><td>'.substr($item["PRICE"],0,-2).'</td><td>'.(float)$item["SUMM"].'</td></tr>';
+                $strOrderList .= '<tr><td>'.$item["PRODUCT_CODE"].'</td><td>'.$item["BRAND"].' - '.$item["NAME"].'</td><td>'.$item["QUANTITY"].'</td><td>'.substr($item["PRICE"],0,-2).'</td><td>'.(float)$item["SUMM"].'</td></tr>';
             }
             $strOrderList .= "</table>";
             $arFields["ORDER_TABLE_ITEMS"] = $strOrderList;
